423468a36cd918a073796182ce8597cd7c799b81
jitlada <jitlada.yo@ku.th>
2024-07-09 12:36:15 +0700

    coded gumball machine class

--- src/GumballMachine.java
+++ src/GumballMachine.java
@@ -1,5 +1,35 @@
 public class GumballMachine {
     private int gumballs;
+    private boolean hasQuarter;
+    private boolean doesSoldOut;
 
+    public GumballMachine(int gumballs) {
+        this.gumballs = gumballs;
+        hasQuarter = false;
+        doesSoldOut = false;
+    }
 
+    public boolean insertQuarter() {
+        if (!hasQuarter && !doesSoldOut) {
+            hasQuarter = true;
+            return true;
+        }
+        return false;
+    }
+
+    public void turnCrank() {
+        gumballs--;
+        if (gumballs == 0) {
+            doesSoldOut = true;
+        }
+        hasQuarter = false;
+    }
+
+    public boolean doesSoldOut() {
+        return doesSoldOut;
+    }
+
+    public int getGumballs() {
+        return gumballs;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+        System.out.println("Mighty Gumball, Inc.");
+        System.out.println("Java-enabled Standing Gumball Model #2004");
+
+        char selected;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            selected = scanner.next().charAt(0);
+            System.out.println("Menu: C)heck inventory I)nsert quarter E)xit");
+            if (selected == 'E' || selected == 'e') {
+                break;
+            } else if (selected == 'C' || selected == 'c') {
+                System.out.println("Inventory: " +
+                        gumballMachine.getGumballs() + "gumball" + (gumballMachine.getGumballs() <= 1? "":"s"));
+
+                System.out.println("Machine is waiting for quarter\n");
+            } else if (selected == 'I' || selected == 'i') {
+                System.out.println("You insert a quarter");
+                if (!gumballMachine.insertQuarter()) {
+                    System.out.println("Quarter returned");
+                }
+
+            }
+        }
+
+
+        System.out.println("You turned...");
+        gumballMachine.turnCrank();
+        System.out.println("A gumball comes rolling out the slot");
+    }
 }
